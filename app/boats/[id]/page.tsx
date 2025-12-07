ea8bc0b584f2a48c2d1ce50b1d142d9db9b4903b
chuongdinh2k <dinhvanchuong2k@gmail.com>
2025-12-07 23:27:30 +0700

    remove api env

--- app/boats/[id]/page.tsx
+++ app/boats/[id]/page.tsx
@@ -7,37 +7,36 @@ export async function generateMetadata({
     params: Promise<{ id: string }>;
 }): Promise<Metadata> {
     const { id } = await params;
-    const backendApiUrl = process.env.BACKEND_API_URL || "https://dev-sbh-be.yellowocean-92ab1503.australiaeast.azurecontainerapps.io/api"
+    const backendApiUrl = "https://dev-sbh-be.yellowocean-92ab1503.australiaeast.azurecontainerapps.io/api"
     try {
-        const url = `${backendApiUrl}/vessels/${id}?populate[vesselPosts][populate][content][populate]=*`;
-        const response = await fetch(url, {
-            method: "GET",
-            next: { revalidate: 3600 },
-        });
-        if (!response.ok) {
-            return {
-                title: `Boat Detail - ${id}`,
-                description: "Discover luxury boat hire options in Sydney Harbour.",
-            };
-        }
+        // const url = `${backendApiUrl}/vessels/${id}?populate[vesselPosts][populate][content][populate]=*`;
+        // const response = await fetch(url, {
+        //     method: "GET",
+        //     next: { revalidate: 3600 },
+        // });
+        // if (!response.ok) {
+        //     return {
+        //         title: `Boat Detail - ${id}`,
+        //         description: "Discover luxury boat hire options in Sydney Harbour.",
+        //     };
+        // }
 
-        const result = await response.json();
-        const vessel = result.data;
-        if (!vessel) {
-            return {
-                title: "Boat Not Found",
-                description: "The requested boat could not be found.",
-            };
-        }
+        // const result = await response.json();
+        // const vessel = result.data;
+        // if (!vessel) {
+        //     return {
+        //         title: "Boat Not Found",
+        //         description: "The requested boat could not be found.",
+        //     };
+        // }
 
-        const vesselName =
-            vessel?.vesselPosts?.content?.seo?.title || vessel?.vesselName + " Boat Hire ";
+        const vesselName = "Blue Sky Boat Hire";
         const description =
-            vessel?.vesselPosts?.content?.seo?.description || vessel?.vesselName + " Boat Hire";
+            "Blue Sky Boat Hire";
         const linkUrl = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.connetweb.com%2Fit%2Fproduct%2Ftest-product-1%2F&psig=AOvVaw0gQEtbULazR2r_ocD7beEz&ust=1764740240682000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCPiWyuyXnpEDFQAAAAAdAAAAABAE"
             return {
             title: `${vesselName}`,
-            description: `${vesselName}`,
+            description: `${description}`,
             // openGraph: {
             //     title: `${vesselName}`,
             //     description: `${vesselName}`,
